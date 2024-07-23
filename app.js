ecfe741d34cf547a9510eb49275202b1dbe4b4b3
Ian Smythe <ianbsmythe@googlemail.com>
2024-07-23 14:11:12 +0100

    Epic 2: user story 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import activities from './activities.json' with {type: "json"}
 
 const app = express();
 const port = 3000;
@@ -12,7 +13,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/activities', (req, res) => {
+  res.send('');
+});
+
 app.listen(port),
   () => {
     console.log(`Example app listening on port ${port}`);
   };
+
