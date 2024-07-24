eb94b1056a9ff5aedcbee33c100cbb5a61e1914c
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-07-24 13:51:16 +0100

    Half way through Create Express GET request handler that takes user_id step

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ import express from 'express';
 import helmet from 'helmet';
 import activities from './activities.json' with {type: "json"}
 
-console.log(activities);
-
 const app = express();
 const port = 3000;
 
-//Middleware
+// Middleware
 app.use(helmet());
 
-//Routes
+// Request handlers
+// Root/default request handler
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/activities', (req, res) => {
+// Activities request handler
+app.get('/activities/:userid', (req, res) => {
+  //console.log(req.params.userid);
+
+  // TODO: Frank is going to finish this!
+  activities.forEach(function (arrayElement) {
+    if (arrayElement.id === "55554444") {
+      console.log(arrayElement);
+    }
+  })
+
+  //const userActivity = activities
+
   res.json(activities);
 });
 
