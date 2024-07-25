b68f67c9b7817bdc3db8a050e6cd883a5859e905
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-07-25 14:50:22 +0100

    Basic implementation of Epic 2 - User Story 1: Implement functionality to get all activities added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
 Node module imports
 */
-// Epic 1 - User Story 1 Set up basic Express application.
+// Epic 1 - User Story 1: Set up basic Express application
 import express from 'express';
 import helmet from 'helmet';
 import activities from './activities.json' with {type: "json"}
@@ -9,16 +9,16 @@ import activities from './activities.json' with {type: "json"}
 /*
 Express application setup
 */
-// using express to create an application
+// Using express to create an application
 const app = express();
 
-// save port in a variable
+// Save port in a variable
 const port = 3000;
 
 /*
 Middleware
 */
-// Epic 1 - User Story 2 Add basic security standards using Helmet
+// Epic 1 - User Story 2: Add basic security standards using Helmet
 app.use(helmet());
 app.use(express.json());
 
@@ -30,16 +30,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Activities GET request handler
+// Activities GET request handler - BASIC VERSION - RETURN ALL ACTIVITIES IN OUR JSON FILE
+// Epic 2 - User Story 1: Implement functionality to get all activities
+app.get('/activities', (req, res) => {
+  // Send the response status code 200 and the data inside the activities array
+  res.status(200);
+  res.json(activities);
+});
+
+// Activities GET request handler - ADVANCED VERSION - RETURN ACTIVITIES FOR A PARTICULAR USER
 // Epic 2 - User Story 1: Implement functionality to get all activities for a particular user
 app.get('/activities/:userid', (req, res) => {
-  // create a variable to capture path paramater from the GET request URL
+  // Create a variable to capture path paramater from the GET request URL
   const userid = req.params.userid;
 
-  // create blank array so we can shove multiple objects into the array
+  // Create blank array so we can shove multiple objects into the array
   let responseData = [];
 
-  /* array method that iterates through the array and for each userid that matches within the json file, 
+  /* Array method that iterates through the array and for each userid that matches within the json file, 
   we push that object into our blank array aka responseData array */
   activities.forEach(function (arrayElement) {
     if (arrayElement.id === userid) {
@@ -47,7 +55,7 @@ app.get('/activities/:userid', (req, res) => {
     }
   });
 
-  // to send the response status code 200 and the response data of the array
+  // Send the response status code 200 and the data inside the activities array
   res.status(200);
   res.json(responseData);
 });
@@ -55,7 +63,7 @@ app.get('/activities/:userid', (req, res) => {
 // Activities POST request handler
 // Epic 2 - User Story 2: Implement functionality to POST a new activity.
 app.post('/activities', (req, res) => {
-  //Whatever the body of the request is, we will save it as 'newActivity'
+  // Whatever the body of the request is, we will save it as 'newActivity'
   let newActivity = req.body;
   res.send('POST processed');
   activities.push(newActivity);
@@ -65,7 +73,7 @@ app.post('/activities', (req, res) => {
 /*
 Express server start
 */
-// spin up the express server
+// Spin up the express server
 app.listen(port),
   () => {
     console.log(`Example app listening on port ${port}`);
